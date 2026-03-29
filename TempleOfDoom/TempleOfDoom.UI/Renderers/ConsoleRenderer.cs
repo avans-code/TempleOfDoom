ec1933fab3794dcf8fb32a166ab921c99c5a9b2d
Just <167076399+jbhuisman@users.noreply.github.com>
2026-03-29 20:09:20 +0200

    .

--- TempleOfDoom/TempleOfDoom.UI/Renderers/ConsoleRenderer.cs
+++ TempleOfDoom/TempleOfDoom.UI/Renderers/ConsoleRenderer.cs
@@ -44,77 +44,94 @@ public class ConsoleRenderer
                     Console.Write("  ");
                 }
             }
+
             Console.WriteLine();
         }
-        
+
         Console.WriteLine("\n--------------------------------------------------");
         Console.WriteLine($"Levens: {level.Player.Lives} | Stenen: {level.Player.StonesCollected}/5");
         Console.WriteLine("--------------------------------------------------");
     }
 
-        private bool TryDrawDoor(Room room, int x, int y, Level level)
-        {
-            bool isDoor = false;
-            char doorChar = ' '; // Default to space for open doors
-            ConsoleColor doorColor = ConsoleColor.White;
-
-            if (y == 0 && x == room.Width / 2 && room.OutgoingConnections.ContainsKey("NORTH")) { isDoor = true; }
-            else if (y == room.Height - 1 && x == room.Width / 2 && room.OutgoingConnections.ContainsKey("SOUTH")) { isDoor = true; }
-            else if (x == 0 && y == room.Height / 2 && room.OutgoingConnections.ContainsKey("WEST")) { isDoor = true; }
-            else if (x == room.Width - 1 && y == room.Height / 2 && room.OutgoingConnections.ContainsKey("EAST")) { isDoor = true; }
-        
-            if (room.SpecialTiles.ContainsKey((x, y)) && room.SpecialTiles[(x, y)] == "innerdoor") { isDoor = true; }
-
-            if (!isDoor) return false;
-
-            Connection? conn = null;
-            if (y == 0) conn = room.OutgoingConnections.GetValueOrDefault("NORTH");
-            else if (y == room.Height - 1) conn = room.OutgoingConnections.GetValueOrDefault("SOUTH");
-            else if (x == 0) conn = room.OutgoingConnections.GetValueOrDefault("WEST");
-            else if (x == room.Width - 1) conn = room.OutgoingConnections.GetValueOrDefault("EAST");
-
-            // Check if connection can be entered (door is open)
-            bool canEnter = conn?.CanEnter(level.Player, room) ?? false;
-            
-if (!canEnter)
-{
-    // Door is closed, show appropriate symbol
-    var doorType = conn?.Doors.FirstOrDefault();
-    
-    switch (doorType)
+    private bool TryDrawDoor(Room room, int x, int y, Level level)
     {
-        case ColoredDoor coloredDoor:
-            doorChar = '=';
-            doorColor = GetConsoleColor(coloredDoor.Color);
-            break;
-        case ClosingGate:
-            doorChar = 'n';
-            break;
-        default:
-            doorChar = '┴';
-            break;
-    }
-}
-            // If canEnter is true, doorChar remains ' ' (space)
-        
-            // Handle inner doors specifically
-            if (room.SpecialTiles.ContainsKey((x, y)) && room.SpecialTiles[(x, y)] == "innerdoor") 
-            { 
-                var plates = room.Entities.OfType<PressurePlate>().ToList();
-                if (plates.Any() && !plates.All(p => p.IsPressed))
-                {
-                    doorChar = '┴'; // Closed inner door
-                }
-                else
-                {
-                    doorChar = ' '; // Open inner door
-                }
-                doorColor = ConsoleColor.White;
+        bool isDoor = false;
+        char doorChar = ' '; // Default to space for open doors
+        ConsoleColor doorColor = ConsoleColor.White;
+
+        if (y == 0 && x == room.Width / 2 && room.OutgoingConnections.ContainsKey("NORTH"))
+        {
+            isDoor = true;
+        }
+        else if (y == room.Height - 1 && x == room.Width / 2 && room.OutgoingConnections.ContainsKey("SOUTH"))
+        {
+            isDoor = true;
+        }
+        else if (x == 0 && y == room.Height / 2 && room.OutgoingConnections.ContainsKey("WEST"))
+        {
+            isDoor = true;
+        }
+        else if (x == room.Width - 1 && y == room.Height / 2 && room.OutgoingConnections.ContainsKey("EAST"))
+        {
+            isDoor = true;
+        }
+
+        if (room.SpecialTiles.ContainsKey((x, y)) && room.SpecialTiles[(x, y)] == "innerdoor")
+        {
+            isDoor = true;
+        }
+
+        if (!isDoor) return false;
+
+        Connection? conn = null;
+        if (y == 0) conn = room.OutgoingConnections.GetValueOrDefault("NORTH");
+        else if (y == room.Height - 1) conn = room.OutgoingConnections.GetValueOrDefault("SOUTH");
+        else if (x == 0) conn = room.OutgoingConnections.GetValueOrDefault("WEST");
+        else if (x == room.Width - 1) conn = room.OutgoingConnections.GetValueOrDefault("EAST");
+
+        // Check if connection can be entered (door is open)
+        bool canEnter = conn?.CanEnter(level.Player, room) ?? false;
+
+        if (!canEnter)
+        {
+            // Door is closed, show appropriate symbol
+            var doorType = conn?.Doors.FirstOrDefault();
+
+            switch (doorType)
+            {
+                case ColoredDoor coloredDoor:
+                    doorChar = '=';
+                    doorColor = GetConsoleColor(coloredDoor.Color);
+                    break;
+                case ClosingGate:
+                    doorChar = 'n';
+                    break;
+                default:
+                    doorChar = '┴';
+                    break;
+            }
+        }
+        // If canEnter is true, doorChar remains ' ' (space)
+
+        // Handle inner doors specifically
+        if (room.SpecialTiles.ContainsKey((x, y)) && room.SpecialTiles[(x, y)] == "innerdoor")
+        {
+            var plates = room.Entities.OfType<PressurePlate>().ToList();
+            if (plates.Any() && !plates.All(p => p.IsPressed))
+            {
+                doorChar = '┴'; // Closed inner door
+            }
+            else
+            {
+                doorChar = ' '; // Open inner door
             }
 
-            DrawChar(doorChar, doorColor);
-            return true;
+            doorColor = ConsoleColor.White;
         }
+
+        DrawChar(doorChar, doorColor);
+        return true;
+    }
 
     private void DrawEntity(Entity entity)
     {
@@ -146,7 +163,7 @@ if (!canEnter)
         Console.ForegroundColor = color;
         Console.Write(c);
         Console.ResetColor();
-        
+
         // Added space for aspect ratio correction
         Console.Write(" ");
     }
