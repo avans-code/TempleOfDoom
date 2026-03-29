7d548d374a5c90968a5288408cfa374005f075db
Just <167076399+jbhuisman@users.noreply.github.com>
2026-03-29 20:09:28 +0200

    .

--- TempleOfDoom/TempleOfDoom.Domain/Enemies/EnemyAdapter.cs
+++ TempleOfDoom/TempleOfDoom.Domain/Enemies/EnemyAdapter.cs
@@ -1,30 +1,34 @@
 using TempleOfDoom.Domain.Models;
-using CODE_TempleOfDoom_DownloadableContent; 
+using CODE_TempleOfDoom_DownloadableContent;
 
 namespace TempleOfDoom.Domain.Enemies;
 
 public class EnemyAdapter : Entity
 {
     private readonly Enemy _dllEnemy;
+    private int _localLives;
 
     public EnemyAdapter(Enemy dllEnemy, int x, int y) : base(x, y)
     {
         _dllEnemy = dllEnemy;
-        _dllEnemy.CurrentField = new FieldAdapter(); //
+        _localLives = _dllEnemy.NumberOfLives; 
+
+        _dllEnemy.CurrentField = new FieldAdapter(); 
+        _dllEnemy.CurrentField.Item = _dllEnemy;
     }
 
     public void Move()
     {
-        _dllEnemy.Move();
-        X = _dllEnemy.CurrentXLocation;
-        Y = _dllEnemy.CurrentYLocation;
+            _dllEnemy.Move();
+            X = _dllEnemy.CurrentXLocation;
+            Y = _dllEnemy.CurrentYLocation;
     }
 
     public void TakeDamage(int damage)
     {
-        _dllEnemy.DoDamage(damage);
+        _localLives -= damage; 
     }
 
-    public int Lives => _dllEnemy.NumberOfLives;
-    public bool IsDead => Lives <= 0;
+    // public int Lives => _localLives;
+    public bool IsDead => _localLives <= 0;
 }
